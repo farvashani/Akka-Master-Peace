4efb80ec27d8e7cf4e8d29bc5888e3adefd66d24
hossein farvashani <hossein@hossein-debian.hossein.debian>
2019-06-07 06:19:20 +0430

    add src to build path

--- src/build_all.sh
+++ src/build_all.sh
@@ -1,10 +1,10 @@
 echo "========================================"
 pwd
-cd ~/Akka-Master-Peace/Akkamart/Akkamart.Home/
+cd ~/Akka-Master-Peace/src/Akkamart/Akkamart.Home/
 sh build_docker.sh
 
 echo "========================================"
 pwd
-cd ~/Akka-Master-Peace/Akkamart/Akkamart.Identity/
+cd ~/Akka-Master-Peace/src/Akkamart/Akkamart.Identity/
 sh build_docker.sh
 echo "========================================"
